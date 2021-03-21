8f085668f5f25d693e68640d0c1e95eb4b910be1
venilla <vennila.ramasami07@gmail.com>
2021-03-21 12:23:32 +0400

    [mul-0.2]Arriver a l'etape 10

--- mulTwolnt.py
+++ mulTwolnt.py
@@ -1,12 +1,22 @@
-def mul(a,b):
-  mul=a*b;
-  return mul;
+#!/usr/bin/env python
+
+def mul_num(a,b):
+        sum que deux=a+b;
+        return sum;
 
 def main():
-    import sys
-    print(sys.argv)
-    x=int(sys.argv[1])
-    y=int(sys.argv[2])
-    print(mul(x,y))
-main()
+        import sys
+        print(sys.argv)
+        i=(len(sys.argv)-1)
+        print("Le nombre d'arguments : ",i)
 
+
+
+        if (i==2):
+               x = int (sys.argv[1])
+               y = int (sys.argv[2])
+               print(mul_num(x,y))
+        else:
+               print("Erreur : Veuillez inserer que deux arguments")
+
+main()
