db17d9ef790ed5f14afb2886ba861e4c5ac687f8
Ashamyu <79833212+Ashamyu@users.noreply.github.com>
2021-03-21 17:28:28 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-
+reponse=str(input("Voulez-vous additionner deux int? :  "))
+if reponse=="oui":
+   from addTwolnt import *
+elif reponse=="non":
+     print("La reponse est non.okay!")
+else:
+     print("La reponse n'est ni OUI ou NON!!!")
+      
 
 reponse=str(input("Voulez-vous multiplier les deux int? : "))
 if reponse=="oui":
