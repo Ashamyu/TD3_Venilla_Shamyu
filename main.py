3bd0615807db7f4e752e6b6d0eb86f881e5525ae
asham <ashamyu5@gmail.com>
2021-03-21 16:12:37 +0400

    [1.0-integration]creation du main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+reponse=str(input("Voulez-vous additionner deux int? :  "))
+if reponse=="oui":
+   from addTwolnt import *
+elif reponse=="non":
+     print("La reponse est non.okay!")
+else:
+     print("La reponse n'est ni OUI ou NON!!!")
+
+
