185deaebaa15e941096c760ba80d43d3b230992f
asham <ashamyu5@gmail.com>
2021-03-21 12:01:17 +0400

    [0.3add]Arriver a l'etape 14

--- addTwolnt.py
+++ addTwolnt.py
@@ -9,9 +9,20 @@ def main():
     i=(len(sys.argv)-1)
     print("le nombre d'arguments : ",i)
 
-    if (i==2):
+    if (i==0):
+           ar1=int(input("Inserez le premier argument : "))
+           ar2=int(input("Inserez le deuzieme argument : "))
+           x=int(ar1)
+           y=int(ar2)
+           print (add(x,y))
+    elif(i==1):
+           ar1=int(input("Inserez le deuzieme argument : "))
            x=int(sys.argv[1])
-           y=int(sys.argv[2])
+           y=int(ar1)
+           print (add(x,y))
+    elif(i==2):
+           x=int(sys.argv[1])
+           x=int(sys.argv[2])
            print (add(x,y))
     else:
            print("erreur : veuiller inserez que deux arguments")
